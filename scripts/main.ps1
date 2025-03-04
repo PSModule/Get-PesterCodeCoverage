32b596d1424b55ee82d064855e21a123511ec6d6
Marius Storhaug <marstor@hotmail.com>
2025-03-05 00:20:35 +0100

    Enhance code coverage report by formatting command output with markdown code fences for better readability

--- scripts/main.ps1
+++ scripts/main.ps1
@@ -61,33 +61,27 @@ foreach ($file in $files) {
     $allMissed += $jsonContent.CommandsMissed
     $allExecuted += $jsonContent.CommandsExecuted
     $allFiles += $jsonContent.FilesAnalyzed
-
-    # Keep track of coverage targets so we can pick the highest
     $allTargets += $jsonContent.CoveragePercentTarget
 }
 
 # -- Remove duplicates from each set --
-# Adjust these properties as necessary for your "unique" definition:
 $finalExecuted = $allExecuted |
     Sort-Object -Property File, Line, Command, StartColumn, EndColumn, Class, Function -Unique
 
-# Normalize them to paths relative to outputs/module
 $finalFiles = $allFiles | Sort-Object -Unique
 
 # -- Remove from missed any command that shows up in executed --
-# Build "keys" for each unique executed command
 $executedKeys = $finalExecuted | ForEach-Object {
     '{0}|{1}|{2}|{3}|{4}|{5}|{6}' -f $_.File, $_.Line, $_.Command, $_.StartColumn, $_.EndColumn, $_.Class, $_.Function
 }
-# Filter out commands from $allMissed that are in $executedKeys
-$finalMissed = $allMissed | Sort-Object -Property File, Line, Command, StartColumn, EndColumn, Class, Function -Unique | Where-Object {
-    $key = '{0}|{1}|{2}|{3}|{4}|{5}|{6}' -f $_.File, $_.Line, $_.Command, $_.StartColumn, $_.EndColumn, $_.Class, $_.Function
-    $executedKeys -notcontains $key
-}
+$finalMissed = $allMissed |
+    Sort-Object -Property File, Line, Command, StartColumn, EndColumn, Class, Function -Unique |
+    Where-Object {
+        $key = '{0}|{1}|{2}|{3}|{4}|{5}|{6}' -f $_.File, $_.Line, $_.Command, $_.StartColumn, $_.EndColumn, $_.Class, $_.Function
+        $executedKeys -notcontains $key
+    }
 
 # -- Compute the new coverage percentages --
-#   CoveragePercent = (Count(Executed) / Count(Executed + Missed)) * 100
-#   Use the highest coverage target from all the files
 $missedCount = $finalMissed.Count
 $executedCount = $finalExecuted.Count
 $totalAnalyzed = $missedCount + $executedCount
@@ -100,25 +94,24 @@ if ($totalAnalyzed -gt 0) {
 
 $coveragePercentTarget = $allTargets | Measure-Object -Maximum | Select-Object -ExpandProperty Maximum
 if (-not $coveragePercentTarget) {
-    # If no coverage targets were found in the files, default to 0 or whatever you choose
     $coveragePercentTarget = 0
 }
 
-# -- Build final coverage object with the specified fields --
+# -- Build final coverage object --
 $codeCoverage = [PSCustomObject]@{
     CommandsMissed        = $finalMissed
     CommandsExecuted      = $finalExecuted
     FilesAnalyzed         = $finalFiles
     CoveragePercent       = $coveragePercent
     CoveragePercentTarget = $coveragePercentTarget
-    CoverageReport        = ''  # "Ignore this; can be generated later"
+    CoverageReport        = ''
     CommandsAnalyzedCount = [Int64]$totalAnalyzed
     CommandsExecutedCount = [Int64]$executedCount
     CommandsMissedCount   = [Int64]$missedCount
     FilesAnalyzedCount    = [Int64]$finalFiles.Count
 }
 
-#Print stats:
+# Print stats:
 $stats = [pscustomobject]@{
     Coverage = "$($codeCoverage.CoveragePercent)% / $($codeCoverage.CoveragePercentTarget)%"
     Analyzed = "$($codeCoverage.CommandsAnalyzedCount) commands"
@@ -142,6 +135,35 @@ LogGroup 'Files analyzed' {
     $codeCoverage.FilesAnalyzed | Format-Table -AutoSize | Out-String
 }
 
+# --------------------------------------------------------------------------------
+# Transform the Command property with markdown code fences before building the table
+# --------------------------------------------------------------------------------
+$missedForDisplay = $codeCoverage.CommandsMissed | Sort-Object -Property File, Line | ForEach-Object {
+    [PSCustomObject]@{
+        File        = $_.File
+        Line        = $_.Line
+        StartColumn = $_.StartColumn
+        EndColumn   = $_.EndColumn
+        Class       = $_.Class
+        Function    = $_.Function
+        # Wrap the command in triple-backtick code fences with "pwsh"
+        Command     = "``````pwsh" + [System.Environment]::NewLine + $_.Command + [System.Environment]::NewLine + "``````"
+    }
+}
+
+$executedForDisplay = $codeCoverage.CommandsExecuted | Sort-Object -Property File, Line | ForEach-Object {
+    [PSCustomObject]@{
+        File        = $_.File
+        Line        = $_.Line
+        StartColumn = $_.StartColumn
+        EndColumn   = $_.EndColumn
+        Class       = $_.Class
+        Function    = $_.Function
+        # Wrap the command in triple-backtick code fences with "pwsh"
+        Command     = "``````pwsh" + [System.Environment]::NewLine + $_.Command + [System.Environment]::NewLine + "``````"
+    }
+}
+
 # -- Output the markdown to GitHub step summary --
 $markdown = Heading 1 'Code Coverage Report' {
 
@@ -152,13 +174,13 @@ $markdown = Heading 1 'Code Coverage Report' {
 
         Details "Missed commands [$($codeCoverage.CommandsMissedCount)]" {
             Table {
-                $codeCoverage.CommandsMissed | Sort-Object -Property File, Line
+                $missedForDisplay
             }
         }
 
         Details "Executed commands [$($codeCoverage.CommandsExecutedCount)]" {
             Table {
-                $codeCoverage.CommandsExecuted | Sort-Object -Property File, Line
+                $executedForDisplay
             }
         }
 
@@ -172,11 +194,7 @@ $markdown = Heading 1 'Code Coverage Report' {
 
 Set-GitHubStepSummary -Summary $markdown
 
-#   TODO: Output the markdown to PR comment
-
-#TODO: Generate a JSON coverage report and upload it as an artifact
-
-# -- Throw an error if coverage is below target --
+# Throw an error if coverage is below target
 if ($coveragePercent -lt $coveragePercentTarget) {
     Write-GitHubError "Coverage is below target! Found $coveragePercent%, target is $coveragePercentTarget%."
     exit 1
